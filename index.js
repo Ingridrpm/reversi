14e1ac0a3a6af6987561104d576b78aac4d64013
Ingridrpm <34289725+Ingridrpm@users.noreply.github.com>
2021-06-22 03:04:21 -0600

    menos uno

--- index.js
+++ index.js
@@ -6,7 +6,10 @@ var app = express();
 app.get('/', function (req, res) {
     var turno = req.query.turno;
     var estado = req.query.estado;
-    res.send(reversi.siguiente_movimiento(turno, estado)+"")
+    var resultado = reversi.siguiente_movimiento(turno, estado)+""
+    var v1 = parseInt(resultado.charAt(0))-1
+    var v2 = parseInt(resultado.charAt(1))-1
+    res.send(v1+""+v2)
 });
 
 var puerto = process.env.PORT || 3000;
