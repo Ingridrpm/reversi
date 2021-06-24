d6286a55df4c2376a71a2b1d03ef5032318efdab
Ingridrpm <34289725+Ingridrpm@users.noreply.github.com>
2021-06-24 03:05:01 -0600

    ya nel

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.get('/', function (req, res) {
 app.get('/info', function (req, res) {
     var info = "Ingrid Pérez Mena";
     info += "<br/>Profundidad de minimax: " + reversi.prof;
-    info += "<br/>version 15"
+    info += "<br/>version 16"
     info += "<br/>No le gano :("
     res.send(info)
 });
--- reversi.js
+++ reversi.js
@@ -14,11 +14,11 @@ function siguiente_movimiento(jugador, estado) {
     for (var i = 0; i < movimientos.length; i++) {
         if (PESO_CASILLAS[movimientos[i]] == 120) return movimientos[i]
     }
-    console.log(movimientos)
+    //console.log(movimientos)
     movimiento_minimax_h1 = minimax(jugador, tablero, prof, peso_por_casilla)
     movimiento_minimax_h2 = minimax(jugador, tablero, prof, puntuacion)
-    console.log(movimiento_minimax_h1[1])
-    console.log(movimiento_minimax_h2[1])
+    //console.log(movimiento_minimax_h1[1])
+    //console.log(movimiento_minimax_h2[1])
 
     var cv = casillas_vacias(tablero);
     if (PESO_CASILLAS[movimiento_minimax_h1[1]] <=-20 || PESO_CASILLAS[movimiento_minimax_h2[1]] <=-20) {
@@ -58,7 +58,7 @@ function siguiente_movimiento(jugador, estado) {
                     }
                 }
             }
-            console.log(movimientos)
+            //console.log(movimientos)
             if (movimientos.length == 1) return movimientos[0]
             if (movimientos.length > 0) return mejor_movimiento_modificado(movimientos, jugador, tablero, puntuacion)[1]
             else return movimiento_minimax_h2[1]
@@ -273,6 +273,13 @@ function mejor_movimiento_modificado(movimientos, jugador, tablero, heuristica) 
     var resultados = []
     for (var i = 0; i < movimientos.length; i++) {
         var m = movimientos[i]
+        if(movimientos.length>1){
+            if(PESO_CASILLAS[m]<=-20) {
+                movimientos.splice(i, 1);
+                i = 0;
+                continue;
+            }
+        }
         var val = heuristica(jugador, mover(m, jugador, [...tablero]));
         resultados.push(val)
         if (mayor[0] < val) {
@@ -281,7 +288,6 @@ function mejor_movimiento_modificado(movimientos, jugador, tablero, heuristica) 
         }
 
     }
-    console.log("mayor"+mayor)
     return mayor
 }
 
